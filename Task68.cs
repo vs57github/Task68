a53de68ee00caccb5f0bc7066962ac173366c3b0
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-24 22:21:57 +0300

    Add files via upload

---
+++ Task68.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AkkermanFunction
+{
+    class Program
+    {
+        static int Akkerman(int m, int n) 
+        { 
+            if (m == 0) 
+                return n + 1; 
+
+            else if (n == 0) 
+                return Akkerman(m - 1, 1); 
+
+            else 
+                return Akkerman(m - 1, Akkerman(m, n - 1)); 
+        }
+
+        static void Main(string[] args) 
+        { 
+            int m = 3; // Неотрицательное число m.  
+            int n = 2; // Неотрицательное число n.  
+
+            Console.WriteLine("Функция Аккермана: " + Akkerman(m, n));  
+
+        }  
+
+    }  
+}
